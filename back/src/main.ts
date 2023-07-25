58203f5d3ec36d306ed7a06b201ffa3086fcf7d4
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2023-07-25 15:18:23 +0200

    fix: docker network added ipam config

--- back/src/main.ts
+++ back/src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.use(cors());
-  await app.listen(8080);
+  await app.listen(8080, process.env.HOSTNAME || 'localhost');
 }
 bootstrap();
