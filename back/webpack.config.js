ba5a13a6b40a64552249aa22857d06755b87f588
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2023-09-19 12:04:19 +0200

    fix: back build .env files

--- back/webpack.config.js
+++ back/webpack.config.js
@@ -1,34 +1,46 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const webpack = require('webpack');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const nodeExternals = require('webpack-node-externals');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const CopyPlugin = require('copy-webpack-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 
-module.exports = {
-  entry: ['./src/main.ts'],
-  target: 'node',
-  externals: [nodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  mode: 'development',
-  resolve: {
-    alias: {
-      '@app': path.resolve(__dirname, 'src'),
+module.exports = function (config) {
+  return {
+    entry: ['./src/main.ts'],
+    target: 'node',
+    externals: [nodeExternals()],
+    module: {
+      rules: [
+        {
+          test: /.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
     },
-
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'dist.js',
-  },
+    mode: 'development',
+    resolve: {
+      alias: {
+        '@app': path.resolve(__dirname, 'src'),
+      },
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+      ...config.plugins,
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, '.env*'),
+            to: path.resolve(__dirname, 'dist'),
+          },
+        ],
+      }),
+    ],
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'dist.js',
+    },
+  };
 };
